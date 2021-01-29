5b578fa69c815f3e1d51a0730a5f930a65c74980
sydneyoconnor <42586605+sydneyoconnor@users.noreply.github.com>
2021-01-29 01:09:53 -0500

    function to identify closest object in lidar scan

---
+++ closest.py
@@ -0,0 +1,46 @@
+#function to find closest object
+import json
+from rplidar import RPLidar
+lidar = RPLidar('/dev/ttyUSB0')
+
+#check to make sure LIDAR is stopped so it can start in correct position
+if(lidar.motor == False):
+    lidar.stop()
+    lidar.stop_motor()
+    lidar.disconnect()
+    lidar = RPLidar('/dev/ttyUSB0')
+
+info = lidar.get_info()
+print(info)
+
+health = lidar.get_health()
+print(health)
+
+def closest_object():
+    int largest_dist = 0
+    int largest_angle = 0
+    # largest = {'distance' : 0, 'theta' : 0}
+    for i, scan in enumerate(lidar.iter_scans()):
+        for blip in scan:
+            #blip[1] = angle, blip[2]=distance
+            if(int(round(blip[1])) > 315 or int(round(blip[1])) < 45):
+                if(blip[2] > largest):
+                    largest_dist = blip[2]
+                    largest_angle = blip[1]
+            # print(largest_dist)
+            largest = {'distance' : largest_dist, 'theta' : largest_angle}
+            json_data = json.dumps(largest)
+            print(json_data)
+            # with open("sample.json", "w") as outfile: 
+            #     json.dump(largest, outfile) 
+        #just for testing purposes
+        if i > 100:
+            break
+
+closest_object()
+
+lidar.stop()
+lidar.stop_motor()
+lidar.disconnect()
+
+quit()
