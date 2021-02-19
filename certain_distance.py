9e43bfed6adac2a9636d76acf7608ba3f41d4336
carinorthrop <50886022+carinorthrop@users.noreply.github.com>
2021-02-19 12:45:50 -0500

    took out json

--- certain_distance.py
+++ certain_distance.py
@@ -1,9 +1,9 @@
-import json
 from rplidar import RPLidar
+
 lidar = RPLidar('/dev/ttyUSB0')
 
-#check to make sure LIDAR is stopped so it can start in correct position
-if(lidar.motor == False):
+# check to make sure LIDAR is stopped so it can start in correct position
+if (lidar.motor == False):
     lidar.stop()
     lidar.stop_motor()
     lidar.disconnect()
@@ -15,25 +15,23 @@ print(info)
 health = lidar.get_health()
 print(health)
 
+array = []
 def certain_distance(distance):
     for i, scan in enumerate(lidar.iter_scans()):
         for blip in scan:
-            #print(blip)
-            if(int(blip[2]) <= distance):
+            # print(blip)
+            if (int(blip[2]) <= distance):
                 # print("Distance: {}".format(str(blip[2])))
-                y= blip[2]
+                y = blip[2]
                 z = blip[1]
-                #only return a range of thetas
+                # only return a range of thetas
                 if (blip[1] >= 315 or blip[1] <= 45):
-                    setinput = {
-                        "distance": y,
-                        "theta": z
-                        }
-                    convert = json.dumps(setinput)
-                    print(convert)
-        
-        #print('%d: Got %d measurments' % (i, len(scan)))
-        #it is just breaking after 100 scans, will need to change this at some point with something
+                    array.append(y)
+                    array.append(z)
+                    print(array)
+
+        # print('%d: Got %d measurments' % (i, len(scan)))
+        # it is just breaking after 100 scans, will need to change this at some point with something
         if i > 100:
             break
 
